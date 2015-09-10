2c9a320801482caf7bb37c54f3cbd1ba62c6ec94
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-09-10 11:27:38 -0500

    fixed agumented IIFE

--- javascripts/AugmentedIIFE.js
+++ javascripts/AugmentedIIFE.js
@@ -1,7 +1,9 @@
-FruitMaker.prototype.slices = function(name, number) {
-  var fruitSlices=[];
-  for (var i=0, i<number, i++) {
-    fruitSlices.push(fruit);
-  }
-  return fruitSlices;
-};
+var FruitMaker = (function(fruitMaker) {
+  fruitMaker.slices = function(name, number) {
+    var fruitSlices=[];
+    for (var i=0; i<number; i++) {
+      fruitSlices.push(name);
+    }
+    return fruitSlices;
+  };
+})(FruitMaker);
